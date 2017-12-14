d6963f1a78dc08526147ed657f22e3121ffd977b
Jaakko Husso <jaakko.husso@futurice.com>
2017-12-14 23:38:24 +0200

    Initial commit

---
+++ lib/boothylicious_sensor.ex
@@ -0,0 +1,3 @@
+defmodule BoothyliciousSensor do
+  
+end
---
+++ test/boothylicious_sensor_test.exs
@@ -0,0 +1,8 @@
+defmodule BoothyliciousSensorTest do
+  use ExUnit.Case
+  doctest BoothyliciousSensor
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
